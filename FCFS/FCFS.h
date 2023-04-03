12205d254243118d5ff22b7c89204577f9cc058e
virzark <101343392+virzark@users.noreply.github.com>
2023-04-03 21:07:17 +0700

    Update FCFS.h

--- FCFS/FCFS.h
+++ FCFS/FCFS.h
@@ -1,1 +1,18 @@
 #include "../header.h"
+
+typedef struct
+{
+    int pid;  // id proses
+    int at;   // arrival time
+    int bt;   // burst time
+    int tuat; // turn around time
+    int wt;   // waiting time
+} data;
+
+void FCFS();
+
+void minPlusFCFS(int x);
+void sorting(int n, data fcfs[n], int x);
+void inputProcess(int n, data fcfs[n]);
+void beginProcess(int n, data fcfs[n]);
+void printProcess(int n, data fcfs[n]);
