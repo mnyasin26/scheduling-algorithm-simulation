acca291791706d357e98acac9bdde41a8e794ce6
unknown <haroldsimarmata@gmail.com>
2023-04-04 01:13:34 +0700

    PrioNonPree Menu&Input

--- PrioSchedulingNonPreemptive/PrioSchedulingNonPreemptive.c
+++ PrioSchedulingNonPreemptive/PrioSchedulingNonPreemptive.c
@@ -5,31 +5,190 @@
 #define COL_LENGTH_READY_TASK 4
 #define COL_LENGTH_LOG_TASK 3
 #define COL_LENGTH_STATISTICS 4
-
+void PSNPmeaning()
+{
+    printf("+----------------------------------------------------------------+\n");
+    printf("|                   RINGKASAN PS NON PREEMPTIVE                  |\n");
+    printf("+----------------------------------------------------------------+\n");
+    printf("|Priority scheduling non-preemptive adalah salah satu metode     |\n");
+    printf("|proses pada sistem operasi yang memprioritaskan proses-proses   |\n");
+    printf("|penjadwalan  yang memiliki prioritas tertinggi.                 |\n");
+    printf("|Pada metode ini, ketika sebuah proses telah dimulai,            |\n");
+    printf("|maka proses tersebut akan berjalan hingga selesai               |\n");
+    printf("|tanpa diinterupsi oleh proses yang prioritasnya lebih tinggi.   |\n");
+    printf("|Dalam priority scheduling non-preemptive,                       |\n");
+    printf("|urutan eksekusi proses ditentukan berdasarkan                   |\n");
+    printf("|nilai prioritas yang diberikan kepada masing-masing proses.     |\n");
+    printf("|Semakin tinggi nilai prioritas suatu proses,                    |\n");
+    printf("|semakin cepat proses tersebut akan diproses dan selesai.        |\n");
+    printf("|Proses dengan nilai prioritas yang lebih rendah                 |\n");
+    printf("|akan menunggu hingga proses dengan nilai prioritas              |\n");
+    printf("|lebih tinggi selesai dijalankan.                                |\n");
+    printf("+----------------------------------------------------------------+\n");
+    getch();
+}
+void PSNPpros()
+{
+    printf("+----------------------------------------------------------------+\n");
+    printf("|                   KELEBIHAN PS NON PREEMPTIVE                  |\n");
+    printf("+----------------------------------------------------------------+\n");
+    printf("|1.  Meningkatkan performa sistem: Metode ini dapat meningkatkan |\n");
+    printf("|    performa sistem dengan memprioritaskan eksekusi             |\n");
+    printf("|    proses-prosesnyang memiliki prioritas tertinggi             |\n");
+    printf("|    sehingga waktu eksekusi menjadi lebih efisien.              |\n");
+    printf("|2.  Meningkatkan responsifitas sistem: Dengan menerapkan        |\n");
+    printf("|    priority scheduling non-preemptive, proses-proses yang      |\n");
+    printf("|    penting dan memiliki prioritas tinggi dapat segera          |\n");
+    printf("|    dieksekusi dan menyelesaikan tugasnya, sehingga             |\n");
+    printf("|    responsifitas sistem menjadi lebih baik.                    |\n");
+    printf("|3.  Memberikan keamanan pada sistem: Dengan memberikan prioritas|\n");
+    printf("|    yang tepat pada proses-proses kritis dan penting, maka      |\n");
+    printf("|    kemanan sistem akan terjaga dan proses-proses tersebut akan |\n");
+    printf("|    segera diproses.|\n");
+    printf("|4.  Menghindari starvation: Dalam metode ini, proses-proses     |\n");
+    printf("|    dengan prioritas rendah tetap mendapat kesempatan untuk     |\n");
+    printf("|    diproses sehingga tidak terjadi starvation atau             |\n");
+    printf("|    proses-proses dengan prioritas rendah terus menerus tertunda|\n");
+    printf("|    eksekusinya.|\n");
+    printf("|5.  Mudah diimplementasikan: Metode ini mudah diimplementasikan |\n");
+    printf("|    karena hanya memerlukan satu algoritma yang sederhana dan   |\n");
+    printf("|    mudah dipahami.|\n");
+    printf("+----------------------------------------------------------------+\n");
+    getch();
+}
+void PSNPcons()
+{
+    printf("+----------------------------------------------------------------+\n");
+    printf("|                  KEKURANGAN PS NON PREEMPTIVE                  |\n");
+    printf("+----------------------------------------------------------------+\n");
+    printf("|1.  Tidak efektif dalam memanfaatkan sumber daya sistem: Dalam  |\n");
+    printf("|    metode ini, proses dengan prioritas tinggi akan diproses    |\n");
+    printf("|    terlebih dahulu, bahkan jika proses tersebut memerlukan     |\n");
+    printf("|    sumber daya sistem yang banyak. Hal ini dapat menyebabkan   |\n");
+    printf("|    sumber daya sistem lainnya terabaikan dan tidak efektif     |\n");
+    printf("|    dalam memanfaatkan sumber daya sistem.|\n");
+    printf("|2.  Tidak sesuai untuk sistem yang dinamis: Jika prioritas suatu|\n");
+    printf("|    proses berubah-ubah seiring waktu, maka metode ini tidak    |\n");
+    printf("|    efektif karena memerlukan pengaturan ulang prioritas setiap |\n");
+    printf("|    kali terjadi perubahan prioritas.|\n");
+    printf("|3.  Memerlukan pengaturan prioritas yang hati-hati: Dalam metode|\n");
+    printf("|    ini, pengaturan prioritas harus dilakukan dengan hati-hati  |\n");
+    printf("|    karena jika prioritas salah ditentukan, maka proses dengan  |\n");
+    printf("|    prioritas rendah bisa mengalami penundaan eksekusi yang lama|\n");
+    printf("|4.  Memerlukan waktu eksekusi yang lebih lama: Metode ini       |\n");
+    printf("|    memerlukan waktu eksekusi yang lebih lama daripada metode   |\n");
+    printf("|    preemptive karena tidak ada interupsi pada proses-proses    |\n");
+    printf("|    yang sedang berjalan.|\n");
+    printf("|5.  Tidak dapat mencegah deadlock: Metode ini tidak dapat       |\n");
+    printf("|    mencegah terjadinya deadlock, yaitu situasi dimana beberapa |\n");
+    printf("|    proses saling memblokir satu sama lain sehingga tidak dapat |\n");
+    printf("|    terselesaikan.|\n");
+    printf("+----------------------------------------------------------------+\n");
+    getch();
+}
 void PrioSchedulingNonPreemptive()
 {
+    int menu;
+    printf("=====================================================\n");
+    printf("=   Welcome to Priority Scheduling NonPreemptive!   =\n");
+    printf("=====================================================\n");
+    printf("\n");
+
+    printf("!!  Type any of the number listed below            !!\n");
+    printf("!!  to find out more about this method             !!\n");
+    printf("\n");
+
+    printf("||  0. What is PS NonPreemptive?                   ||\n");
+    printf("||  1. PS NonPreemtive Pros                        ||\n");
+    printf("||  2. PS NonPreemtive Cons                        ||\n");
+    printf("||  3. PS NonPreemtive Simulation                  ||\n");
+    printf("||  4. Exit                                        ||\n");
+    printf("\n");
+
+    printf("-= Choose what to do =-:\n");
+    scanf("%d", &menu);
+    switch (menu)
+    {
+    case 0:
+        system("cls");
+        PSNPmeaning();
+        break;
+    case 1:
+        system("cls");
+        PSNPpros();
+        break;
+    case 2:
+        system("cls");
+        PSNPcons();
+        break;
+    case 3:
+        system("cls");
+        PrioSchedulingNonPreemptiveInput();
+        break;
+    case 4:
+        exit(0);
+
+    default:
+        break;
+    }
+}
+
+void PrioSchedulingNonPreemptiveInput()
+{
+    // set banyaknya proses
+    int banyakproses;
+    printf("Berapa banyak proses: ");
+    scanf("%d", &banyakproses);
+
+    process Pp[banyakproses];
+
+    // input dari proses id, priority, arrival dan burst time
+    for (int i = 0; i < banyakproses; i++)
+    {
+        printf("Proses ke-%d\n", i);
+        scanf("%d", &Pp[i].process_id);
+        scanf("%d", &Pp[i].priority);
+        scanf("%d", &Pp[i].arrival_time);
+        scanf("%d", &Pp[i].burst_time);
+        Pp[i].burstTimeLeft = 0;
+        Pp[i].waiting_time = 0;
+        Pp[i].turnaround_time = 0;
+    }
+
+    // set burstimeleft, waiting sama turnaround ke 0
+    // for (int i = 0; i < banyakproses; i++)
+    // {
+    //     Pp[i].burstTimeLeft = 0;
+    //     Pp[i].waiting_time = 0;
+    //     Pp[i].turnaround_time = 0;
+    // }
 
     // Executor bertindak sebagai "processor" yang bertugas untuk mengeksekusi process
     processor executor = {executor.systemTime = 0, executor.state = 1};
 
     // Process p-n adalah perwujudan dari proses dengan berbagai atributnya
-    process p0 = {p0.process_id = 0, p0.priority = 2, p0.burst_time = 2, p0.burstTimeLeft = 0, p0.arrival_time = 0, p0.waiting_time = 0, p0.turnaround_time = 0, p0.response_time = 0};
-    process p1 = {p1.process_id = 1, p1.priority = 3, p1.burst_time = 6, p1.burstTimeLeft = 0, p1.arrival_time = 1, p1.waiting_time = 0, p1.turnaround_time = 0, p1.response_time = 0};
-    process p2 = {p2.process_id = 2, p2.priority = 1, p2.burst_time = 4, p2.burstTimeLeft = 0, p2.arrival_time = 2, p2.waiting_time = 0, p2.turnaround_time = 0, p2.response_time = 0};
+    // process p0 = {p0.process_id = 0, p0.priority = 2, p0.burst_time = 2, p0.burstTimeLeft = 0, p0.arrival_time = 0, p0.waiting_time = 0, p0.turnaround_time = 0};
+    // process p1 = {p1.process_id = 1, p1.priority = 3, p1.burst_time = 6, p1.burstTimeLeft = 0, p1.arrival_time = 1, p1.waiting_time = 0, p1.turnaround_time = 0};
+    // process p2 = {p2.process_id = 2, p2.priority = 1, p2.burst_time = 4, p2.burstTimeLeft = 0, p2.arrival_time = 2, p2.waiting_time = 0, p2.turnaround_time = 0};
 
     // printf("%s",temp);
     // set BurstTimeLeft untuk keperluan pemrosesan
-    p0.burstTimeLeft = p0.burst_time;
-    p1.burstTimeLeft = p1.burst_time;
-    p2.burstTimeLeft = p2.burst_time;
+    // p0.burstTimeLeft = p0.burst_time;
+    // p1.burstTimeLeft = p1.burst_time;
+    // p2.burstTimeLeft = p2.burst_time;
 
     // buat queue untuk tempat penyimpanan sementara untuk process (bukan ready queue)
     queue listProcess;
     createEmpty(&listProcess);
 
-    add(p0, &listProcess);
-    add(p1, &listProcess);
-    add(p2, &listProcess);
+    for (int i = 0; i < banyakproses; i++)
+    {
+        add(Pp[i], &listProcess);
+    }
+
+    // add(p0, &listProcess);
+    // add(p1, &listProcess);
+    // add(p2, &listProcess);
 
     // panggil prosedur simulasi
     simulation(&executor, &listProcess);
@@ -256,10 +415,10 @@ void simulation(processor *executor, queue *listProcess)
     // printf("system time : %d\n", executor->systemTime);
     printf("\nStatistics\n");
 
-    avgWaitingTime /= processDone.last+1;
-    avgTurnAroundTime /= processDone.last+1;
-    avgResponseTime /= processDone.last+1;
-    throughput /= executor->systemTime-1;
+    avgWaitingTime /= processDone.last + 1;
+    avgTurnAroundTime /= processDone.last + 1;
+    avgResponseTime /= processDone.last + 1;
+    throughput /= executor->systemTime - 1;
 
     char temp[50];
     sprintf(temp, "%.2f", throughput);
