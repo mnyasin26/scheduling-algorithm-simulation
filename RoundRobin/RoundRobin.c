a1eb7a9550221845d6041889aaff2d5fad156598
hilmyrs123 <hilmyrs123@gmail.com>
2023-04-03 22:41:12 +0700

    rr

--- RoundRobin/RoundRobin.c
+++ RoundRobin/RoundRobin.c
@@ -1,26 +1,79 @@
 #include "RoundRobin.h"
 
+void kelebihan()
+{
+    printf("+================================================================+\n");
+    printf("|                   KELEBIHAN Round Robin                        |\n");
+    printf("+================================================================+\n");
+    printf("| 1. Tidak menghadapi masalah starvation atau convoy effect.     |\n");
+    printf("| 2. Adil: Karena giliran antar proses sudah diatur oleh         |\n");
+    printf("|    Quantum Time.                                               |\n");
+    printf("| 3. Tidak bergantung pada Burst Time sehingga mudah             |\n");
+    printf("|    diimplementasikan pada sistem                               |\n");
+    printf("+================================================================+\n\n");
+}
+
+void kekurangan()
+{
+    printf("+================================================================+\n");
+    printf("|                   KEKURANGAN Round Robin                       |\n");
+    printf("+================================================================+\n");
+    printf("| 1. Response time dan waiting time yang lebih besar :           |\n");
+    printf("|    Hal tersebut disebabkan oleh pemberian jatah waktu          |\n");
+    printf("|    yang tetap untuk semua proses.                              |\n");
+    printf("| 2. Throughput rendah : Dikarenakan pemberian jatah waktu       |\n");
+    printf("|    yang tetap, throughput rata-rata lebih rendah dari          |\n");
+    printf("|    algoritma penjadwalan lain.                                 |\n");
+    printf("| 3. Tidak memprioritaskan proses yang lebih penting            |\n");
+    printf("| 4. Memakan banyak waktu bila Quantum Timenya kecil.            |\n");
+    printf("| 5. Sulit menemukan Quantum Time yang tepat.                    |\n");
+    printf("|    diimplementasikan pada sistem                               |\n");
+    printf("+================================================================+\n\n");
+}
+
 void RoundRobin()
 {
-    // initlialize the variable name
+    // Variabel
+    /*
+    i = iterator
+    NOP = Number of Process
+    Sum = jumlah burst time
+    Count = untuk menyatakan kondisi proses, bila sudah dikerjakan count menjadi 0, bila belum count = 1
+    y = untuk assign nop
+    quant = Quantum Time
+    wt = waiting time
+    tat = turnaround time
+    at = arrival time
+    bt = burst time
+    temp = temporary array
+    avg_wt = average waiting time
+    avg_tat = average turnarount time*/
     int i, NOP, sum = 0, count = 0, y, quant, wt = 0, tat = 0, at[10], bt[10], temp[10];
     float avg_wt, avg_tat;
-    printf(" Masukkan Jumlah Proses : ");
+    // kelebihan
+    Sleep(800);
+    kelebihan();
+    // kekurangan
+    Sleep(800);
+    kekurangan();
+
+    // mulai input
+    printf("Masukkan Jumlah Proses : ");
     scanf("%d", &NOP);
     y = NOP; // Assign the number of process to variable y
 
     // Use for loop to enter the details of the process like Arrival time and the Burst Time
     for (i = 0; i < NOP; i++)
     {
-        printf("\n Masukkan Arrival Time dan Burst Time tiap proses [%d]\n", i + 1);
-        printf(" Arrival time : \t"); // Accept arrival time
+        printf("Masukkan Arrival Time dan Burst Time tiap proses [%d]\n", i + 1);
+        printf("Arrival time : "); // Accept arrival time
         scanf("%d", &at[i]);
-        printf(" \nBurst time : \t"); // Accept the Burst time
+        printf("Burst time   : "); // Accept the Burst time
         scanf("%d", &bt[i]);
         temp[i] = bt[i]; // store the burst time in temp array
     }
-    // Accept the Time qunat
-    printf("Masukkan Quantum Time : \t");
+    // Accept the Time quantum
+    printf("Masukkan Quantum Time : ");
     scanf("%d", &quant);
     // Display the process No, burst time, Turn Around Time and the waiting time
     printf("\n Process No \t\t Burst Time \t\t TAT \t\t Waiting Time ");
@@ -40,7 +93,9 @@ void RoundRobin()
         if (temp[i] == 0 && count == 1)
         {
             y--; // decrement the process no.
-            printf("\nProcess No[%d] \t\t %d\t\t\t\t %d\t\t\t %d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
+            Sleep(800);
+            printf("\nProcess No[%d] \t\t %d \t\t\t %d \t\t\t %d", i + 1, bt[i], sum - at[i], sum - at[i] - bt[i]);
+            Sleep(800);
             wt = wt + sum - at[i] - bt[i];
             tat = tat + sum - at[i];
             count = 0;
@@ -61,7 +116,11 @@ void RoundRobin()
     // represents the average waiting time and Turn Around time
     avg_wt = wt * 1.0 / NOP;
     avg_tat = tat * 1.0 / NOP;
-    printf("\n Average Turn Around Time: \t%f", avg_wt);
-    printf("\n Average Waiting Time: \t%f", avg_tat);
+    printf("\n");
+    printf("-------------------------------------------------\n");
+    printf("Average Turn Around Time: %f \n", avg_wt);
+    printf("Average Waiting Time: %f \n", avg_tat);
+    printf("-------------------------------------------------\n");
+    printf("Pencet Tombol apapun untuk kembali ke Menu \n");
     getch();
 }
--- RoundRobin/RoundRobin.h
+++ RoundRobin/RoundRobin.h
@@ -1,3 +1,5 @@
 #include "../header.h"
 
 void RoundRobin();
+void kelebihan();
+void kekurangan();
